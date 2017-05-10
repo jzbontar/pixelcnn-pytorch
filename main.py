2bf97108c6ffa294c8c9d15d70dd0f3d4a293936
Jure Zbontar <jure.zbontar@gmail.com>
2017-05-10 12:03:19 -0400

    Simplify

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #! /usr/bin/env python
 
-import argparse
 import os
 import sys
 
@@ -11,14 +10,8 @@ from torch import nn, optim, cuda, backends
 from torch.autograd import Variable
 from torch.utils import data
 from torchvision import datasets, transforms, utils
-
-parser = argparse.ArgumentParser()
-parser.add_argument('action', choices={'train', 'sample'})
-parser.add_argument('--feature-maps', type=int, default=32)
-parser.add_argument('--num-layers', type=int, default=4)
-parser.add_argument('--learning-rate', type=float, default=0.01)
-args = parser.parse_args()
 backends.cudnn.benchmark = True
+
 
 class MaskedConv2d(nn.Conv2d):
     def __init__(self, mask_type, *args, **kwargs):
@@ -27,47 +20,46 @@ class MaskedConv2d(nn.Conv2d):
         self.register_buffer('mask', self.weight.data.clone())
         _, _, kH, kW = self.weight.size()
         self.mask.fill_(1)
-        self.mask[:,:,kH // 2, kW // 2 + (mask_type == 'B'):] = 0
-        self.mask[:,:,kH // 2 + 1:] = 0
+        self.mask[:, :, kH // 2, kW // 2 + (mask_type == 'B'):] = 0
+        self.mask[:, :, kH // 2 + 1:] = 0
 
     def forward(self, x):
         self.weight.data *= self.mask
         return super(MaskedConv2d, self).forward(x)
 
-
-layers = [MaskedConv2d('A',  1, args.feature_maps, 7, 1, 3), nn.ReLU(True)]
-for i in range(args.num_layers):
-    layers.extend([MaskedConv2d('B', args.feature_maps, args.feature_maps, 7, 1, 3), nn.ReLU(True)])
-layers.append(MaskedConv2d('B', args.feature_maps, 256, 7, 1, 3))
-net = nn.Sequential(*layers)
+fm = 32
+net = nn.Sequential(
+    MaskedConv2d('A', 1,   fm, 7, 1, 3), nn.ReLU(True),
+    MaskedConv2d('B', fm,  fm, 7, 1, 3), nn.ReLU(True),
+    MaskedConv2d('B', fm,  fm, 7, 1, 3), nn.ReLU(True),
+    MaskedConv2d('B', fm,  fm, 7, 1, 3), nn.ReLU(True),
+    MaskedConv2d('B', fm,  fm, 7, 1, 3), nn.ReLU(True),
+    MaskedConv2d('B', fm, 256, 7, 1, 3))
 net.cuda()
 
 tr = data.DataLoader(datasets.MNIST('data', train=True, download=True, transform=transforms.ToTensor()),
                      batch_size=128, shuffle=True, num_workers=1, pin_memory=True)
+sample = torch.Tensor(144, 1, 28, 28).cuda()
+optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9, nesterov=True)
+for epoch in range(50):
+    # train
+    err = []
+    for input, _ in tr:
+        input = Variable(input.cuda(async=True))
+        target = Variable((input.data[:,0] * 255).long())
+        loss = F.cross_entropy(net(input), target)
+        err.append(loss.data[0])
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-if args.action == 'train':
-    optimizer = optim.SGD(net.parameters(), lr=args.learning_rate, momentum=0.9, nesterov=True)
-    for epoch in range(10):
-        err = []
-        for input, _ in tr:
-            bs, _, w, h = input.size()
-            input = Variable(input.cuda(async=True))
-            target = Variable((input.data * 255).long())
-            loss = F.cross_entropy(net(input), target.view(bs, w, h))
-            err.append(loss.data[0])
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-        print epoch, np.mean(err)
-        torch.save(net.state_dict(), 'net.pt')
-
-if args.action == 'sample':
-    net.load_state_dict(torch.load('net.pt'))
-    sample = torch.Tensor(64, 1, 28, 28).cuda()
+    # sample
     sample.fill_(0)
     for i in range(28):
         for j in range(28):
             out = net(Variable(sample, volatile=True))
-            probs = F.softmax(out[:,:,i,j]).data
-            sample[:,:,i,j] = torch.multinomial(probs, 1).float() / 255.
-    utils.save_image(sample, 'sample.png')
+            probs = F.softmax(out[:, :, i, j]).data
+            sample[:, :, i, j] = torch.multinomial(probs, 1).float() / 255.
+    utils.save_image(sample, 'sample_{:02d}.png'.format(epoch), nrow=12)
+
+    print epoch, np.mean(err)
